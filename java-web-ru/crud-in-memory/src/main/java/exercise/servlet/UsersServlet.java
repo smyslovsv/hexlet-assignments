a536ade81ea773a4c1978f6eacfc3c7f1777c42b
@hexlet/cli <support@hexlet.io>
2023-05-03 16:13:51 +0300

    submit java-web-ru/crud-in-memory

--- java-web-ru/crud-in-memory/src/main/java/exercise/servlet/UsersServlet.java
+++ java-web-ru/crud-in-memory/src/main/java/exercise/servlet/UsersServlet.java
@@ -205,18 +205,17 @@ public class UsersServlet extends HttpServlet {
         updatedUser.put("id", id);
 
         if (updatedUser.get("lastName").isEmpty() || updatedUser.get("firstName").isEmpty()) {
-            response.setStatus(422);
+
             request.setAttribute("user", updatedUser);
             request.setAttribute("id", id);
             request.setAttribute("error", "Проверьте данные формы");
-            response.sendRedirect("/users/edit?id=" + id);
-//            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/users/edit.jsp?id=" + id);
-//            requestDispatcher.forward(request, response);
-
-        } else {
-            users.set(users.indexOf(user), updatedUser);
-            response.sendRedirect("/users/show?id=" + id);// END
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/edit.jsp");
+            response.setStatus(422);
+            requestDispatcher.forward(request, response);
+            return;
         }
+            users.set(users.indexOf(user), updatedUser);
+            response.sendRedirect("/users");// END
     }
 
     private void deleteUser(HttpServletRequest request,

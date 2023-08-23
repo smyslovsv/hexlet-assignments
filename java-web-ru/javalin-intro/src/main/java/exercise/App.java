b51a1ede643feefc1046f1412f37813ad0e7c168
@hexlet/cli <support@hexlet.io>
2023-08-23 23:11:42 +0300

    submit java-web-ru/javalin-intro

--- java-web-ru/javalin-intro/src/main/java/exercise/App.java
+++ java-web-ru/javalin-intro/src/main/java/exercise/App.java
@@ -9,13 +9,10 @@ public final class App {
     public static Javalin getApp() {
 
         // BEGIN
-        // Создаем приложение
         var app = Javalin.create(config -> {
-            // Включаем логгирование при разработке
             config.plugins.enableDevLogging();
         });
 
-        // Описываем, что будет происходить при GET запросе на адрес /
         app.get("/welcome", ctx -> ctx.result("Welcome to Hexlet!"));
 
         return app;
